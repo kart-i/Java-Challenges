5f9be86f61de03fda87f935142adfa3b48a5e9cd
Karthikeyan Srinivasan <kartheekjai@gmail.com>
2024-11-25 00:12:38 +0530

    Add files via upload

---
+++ Basics/CheckPalindromeForInteger.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class CheckPalindromeForInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Basics/FindQuotient.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class FindQuotient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
