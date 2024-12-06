384ef40d3b8b99ecd5d2f7b5bac2b9cdd082f095
Karthikeyan Srinivasan <kartheekjai@gmail.com>
2024-12-06 11:58:26 +0530

    Update GetCommonPrefix.java

--- StringRelatedChallenges/GetCommonPrefix.java
+++ StringRelatedChallenges/GetCommonPrefix.java
@@ -1,10 +1,33 @@
 package StringRelatedChallenges;
 
 public class GetCommonPrefix {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public String getCommonPrefix(String[] stringArray) {
+		int start = 0;
+		boolean matches = true;
+		String subToCheck = "";
+	
+		for(int end=stringArray[0].length();end>0;end--){
+			for(int j=1;j<stringArray.length;j++) {
+				matches = true;
+				subToCheck= stringArray[0].substring(start,end);
+				if(!stringArray[j].startsWith(subToCheck)) {
+					matches = false;
+					break;
+				}	
+			}if(matches) {
+				return subToCheck;
+			}
+		 }	
+		 return "\"\"";
 	}
 
+	public static void main(String[] args) {
+		GetCommonPrefix obj = new GetCommonPrefix();
+		String[] stringArray = new String[3];
+		stringArray[0] = "wlower";
+		stringArray[1] = "dlow";
+		stringArray[2] = "glowght";
+		
+		System.out.println(obj.getCommonPrefix(stringArray));	
+	}
 }
