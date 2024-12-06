c02a038ce5f650489c410b4e373d3b6eeefad3d2
Karthikeyan Srinivasan <kartheekjai@gmail.com>
2024-12-06 11:55:07 +0530

    Update countOfJewelInStones.java

--- StringRelatedChallenges/countOfJewelInStones.java
+++ StringRelatedChallenges/countOfJewelInStones.java
@@ -1,10 +1,24 @@
 package StringRelatedChallenges;
-
 public class countOfJewelInStones {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int countOfJewelsInStone(String jewels, String stones) {
+	   if(jewels == null || stones == null || jewels.isEmpty() || stones.isEmpty() ) {
+	     return 0;
+	   }
+	   char[] jewelType = jewels.toCharArray();
+	   char[] stoneType = stones.toCharArray();
+	   int count =0;
+	   for(char c : jewelType) {
+	     for(int i=0;i<stoneType.length;i++) {
+	       if(c==stoneType[i]) {
+		count++;
+	       } 	
+	     }
+	   }
+	   return count;
 	}
-
+		
+	public static void main(String[] args) {
+	  countOfJewelInStones obj = new countOfJewelInStones();
+	  System.out.println(obj.countOfJewelsInStone("z", "ZZ"));
+	}
 }
