5049673ddee17eb5996026d4b4db6065811894a4
Karthikeyan Srinivasan <kartheekjai@gmail.com>
2024-12-07 19:48:33 +0530

    Update GetLongestPalindromeSubstring.java

--- StringRelatedChallenges/GetLongestPalindromeSubstring.java
+++ StringRelatedChallenges/GetLongestPalindromeSubstring.java
@@ -1,10 +1,50 @@
 package StringRelatedChallenges;
 
 public class GetLongestPalindromeSubstring {
+	
+	/* Solve using expand around the center technique
+	 * */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public String getLongestPalindromicSubstring(String inputString) {//baaba
+		int start = 0;
+		int end = 0;
 
+		/* Palindrome may be of odd length or even length.
+		 * So, find and compare both the length to find the maximum.
+		 */
+		for(int mid=0;mid<inputString.length();mid++) {
+			// middle value for odd length palindrome will be one digit
+			int oddPalindromeLength = getPalindromeLength(inputString,mid,mid);
+
+			// middle value for even length palindrome will be two digit
+			int evenPalindromeLength = getPalindromeLength(inputString,mid,mid+1);
+			int palindromeLength = Math.max(oddPalindromeLength, evenPalindromeLength);
+
+			/*	intially the first character is set as palindrome
+	 		 *	find the new palindrome start and end.
+			 */
+			if(palindromeLength> end - start) {
+				start = mid-(palindromeLength-1)/2;
+				end = mid+palindromeLength/2;   
+			}
+
+		}
+		return inputString.substring(start, end+1);
 	}
 
+	// expand from the centre to find the palindrome substring
+	public int getPalindromeLength(String s, int left, int right){
+		while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){	
+			left--;
+			right++;
+		}
+		return right-left-1;
+	}
+	
+	public static void main(String[] args) {
+		GetLongestPalindromeSubstring obj = new GetLongestPalindromeSubstring();
+		System.out.println(obj.getLongestPalindromicSubstring("baaba"));
+
+	}
+	
 }
